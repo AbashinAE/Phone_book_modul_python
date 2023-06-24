1ecd46488929cf6a01a0179598fde6f706bbe04e
AbashinAE <a.abashin@list.ru>
2023-06-24 07:46:24 +0300

    add 7 point

---
+++ controller.py
@@ -0,0 +1,51 @@
+import text
+import view
+import model
+
+
+def start():
+    pb = model.PhoneBook()
+    while True:
+        select = view.menu()
+        match select:
+            case 1:
+                if pb.open_file():
+                    view.print_message(text.load_successful)
+                else:
+                    view.print_message(text.error_load)
+            case 2:
+                if pb.save_file():
+                    view.print_message(text.save_successful)
+                else:
+                    view.print_message(text.error_save)
+            case 3:
+                view.show_contacts(pb.get(), text.empty_book)
+            case 4:
+                new = view.add_contact()
+                pb.add_contact(new)
+                view.print_message(text.add_successful(new.get('name')))
+            case 5:
+                word = view.view_input(text.search_word)
+                result = pb.search(word)
+                view.show_contacts(result, text.empty_search(word))
+            case 6:
+                view.show_contacts(pb.get(), text.empty_book)
+                act_id = pb.search_id(text.index_change)
+                change_list = view.change(text.contact_change)
+                pb.change_contact(act_id, change_list)
+                view.print_message(text.change_successful())
+            case 7:
+                word = view.view_input(text.search_word)
+                result = pb.search(word)
+                view.show_contacts(result, text.empty_search(word))
+                index = view.view_input(text.index_remove)
+                name = pb.remove(index)
+                view.print_message(text.remove_contact(name))
+
+            case 8:
+                if pb.check_on_exit():
+                    answer = view.view_input(text.change_confirm)
+                    if answer != 'n':
+                        pb.save_file()
+                view.print_message(text.goodbye)
+                break
---
+++ model.py
@@ -0,0 +1,75 @@
+import json
+import text
+
+
+class PhoneBook:
+
+    def __init__(self, path: str = 'phones.json'):
+        self.contact: dict = {}
+        self.not_changed = {}
+        self.path = path
+
+    def get(self, index: int | None = None):
+        if index:
+            return self.contact.get(index)
+        return self.contact
+
+    def open_file(self):
+        try:
+            with open(self.path, 'r', encoding='UTF-8') as file:
+                self.contact = json.load(file)
+                self.not_changed = self.contact.copy()
+            return True
+        except:
+            return False
+
+    def save_file(self):
+        try:
+            with open(self.path, 'w', encoding='UTF-8') as file:
+                json.dump(self.contact, file, ensure_ascii=False, indent=4)
+            return True
+        except:
+            return False
+
+    def search(self, word: str) -> dict[int:dict[str, str]]:
+        result = {}
+        for index, contact in self.contact.items():
+            if word.lower() in ' '.join(contact.values()).lower():
+                result[index] = contact
+        return result
+
+    def check_id(self):
+        if self.contact:
+            return max(list(map(int, self.contact))) + 1
+        return 1
+
+    def add_contact(self, new: dict[str, str]):
+        contact = {self.check_id(): new}
+        self.contact.update(contact)
+
+    def search_id(self, word: str):
+        while True:
+            id = input(text.index_change)
+            if id in self.contact:
+                return id
+            else:
+                print(text.not_id(id))
+                continue
+
+    def change_contact(self, id: str, contact: []) -> dict[int:dict[str, str]]:
+        if id in self.contact:
+            self.contact[id]['name'] = contact[0]
+            self.contact[id]['phone'] = contact[1]
+            self.contact[id]['comment'] = contact[2]
+            return self.contact
+        else:
+            return text.not_id(id)
+
+    def remove(self, index):
+        name = self.contact.pop(str(index))
+        return name.get('name')
+
+    def check_on_exit(self):
+        if self.contact == self.not_changed:
+            return False
+        return True
